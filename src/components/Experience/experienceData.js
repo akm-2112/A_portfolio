72df0125e7dc3b8d5c81c16e73931fbf2fcc93f1
Aung Kyaw Moe <akyawmoe394@gmail.com>
2026-04-30 20:51:34 +0630

    Add new experience

--- src/components/Experience/experienceData.js
+++ src/components/Experience/experienceData.js
@@ -8,4 +8,13 @@ export const experiences = [
         technologies: ["Laravel", "Vue.js", "Flutter"],
     },
 
+    {
+        id: 2,
+        company: "GMBF GROUP PTE LTD.",
+        role: "Web Developer",
+        date: "Present",
+        description: "Developing scalable and responsive web applications utilizing modern frameworks such as Laravel, Vue.js, and Flutter. Collaborating closely with cross-functional teams to design, implement, and maintain high-quality backend and frontend solutions.",
+        technologies: ["Laravel", "Vue.js", "Flutter"],
+    },
+
 ];
